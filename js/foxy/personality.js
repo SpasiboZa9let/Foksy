e86b62db520b4ca292979d66224949590f19705e
Иван <foxneversleep@gmail.com>
2025-07-06 11:01:31 +0300

    Update personality.js

--- js/foxy/personality.js
+++ js/foxy/personality.js
@@ -1,1 +1,10 @@
+// js/foxy/personality.js
+
+export const emoji = "🦊";
+export const name  = "Фокси";
+
+// Ссылка на Pinterest для дизайна
+export const pinterestLink =
+  "https://ru.pinterest.com/foksynails/дизайн/?" +
+  "invite_code=be24647141714804b78fe8d043c1d5bf&sender=918171580188790185";
 
